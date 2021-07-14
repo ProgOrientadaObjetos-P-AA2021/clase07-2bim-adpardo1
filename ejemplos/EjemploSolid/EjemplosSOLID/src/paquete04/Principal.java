d7e487c1f5b086f31bc27d5efd7743f8b3cb9a52
adpardo1 <73541227+adpardo1@users.noreply.github.com>
2021-07-14 16:23:58 -0500

    07142021

--- ejemplos/EjemploSolid/EjemplosSOLID/src/paquete04/Principal.java
+++ ejemplos/EjemploSolid/EjemplosSOLID/src/paquete04/Principal.java
@@ -17,30 +17,34 @@ public class Principal {
         TransporteBus bus = new TransporteBus();
         bus.establecerCooperativaBus("24 Mayo");
         bus.establecerTarifa();
-        transportes.add(bus);
+        
         TransporteTaxi taxi = new TransporteTaxi();
-        taxi.establecerCooperativaTaxi("Yahuarcuna");
+        taxi.establecerCooperativaTaxi("COOP 2");
         taxi.establecerTarifa();
-        transportes.add(taxi);
         
         TransporteTransvia t3 = new TransporteTransvia();
-        t3.establecerCooperativaTransvia("COOP 1");
+        t3.establecerCooperativaTransvia("COOP 3");
         t3.establecerTarifa();
-        transportes.add(t3);
         
         TransporteAereo aereo = new TransporteAereo();
-        aereo.establecerCooperativaAerea("COOP 2");
+        aereo.establecerCooperativaAerea("COOP 4");
         aereo.establecerTarifa();
-        transportes.add(aereo);
         
         TransporteMaritimo mar = new TransporteMaritimo();
-        mar.establecerCooperativaMaritima("COOP 3");
+        mar.establecerCooperativaMaritima("COOP 5");
         mar.establecerTarifa();
-        transportes.add(mar);
+        
+        ArrayList<Transporte> lista = new ArrayList<>();
+        lista.add(bus);
+        lista.add(taxi);
+        lista.add(t3);
+        lista.add(aereo);
+        lista.add(mar);
         
         TiposTransporte tipos = new TiposTransporte();
+        tipos.obtenerPromedioTarifas();
+        tipos.establecerTransportes(lista);
         tipos.establecerPromedioTarifas();
-
         
         System.out.printf("Promedio de Tarifas: %.2f", 
                 tipos.obtenerPromedioTarifas());
