379b7b7bf151f95de828d31b6105a1c0d63cd39f
adpardo1 <73541227+adpardo1@users.noreply.github.com>
2021-07-15 16:21:52 -0500

    GRUPO 1

---
+++ ejemplos/EjemploSolid/EjemplosSOLID/src/paquete09/APIMovie.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete09;
+
+import paquete10.*;
+
+/**
+ *
+ * @author reroes
+ */
+public interface APIMovie implements API{
+    
+    public void establecerApiKey(String ak);
+    
+    public String obtenerApiKey();
+    
+}
--- ejemplos/EjemploSolid/EjemplosSOLID/src/paquete09/Principal.java
+++ ejemplos/EjemploSolid/EjemplosSOLID/src/paquete09/Principal.java
@@ -17,5 +17,23 @@ public class Principal {
         gp.establecerLlave(api);
         gp.establecerUrl("http://api.movie?api=");
         System.out.println(gp.obtenerUrl());
+        
+        APIDisney api1 = new APIDisney();
+        api2.establecerApiKey("dis0012");
+        
+        GeneradorPeliculas api1 = new GeneradorPeliculas();
+        api1.establecerLlave(api3);
+        api1.establecerUrl("http://api.movie?api=");
+        System.out.println(api1.obtenerUrl());
+
+        
+        APIDisney api3 = new APIDisney();
+        api2.establecerApiKey("dis0012");
+        
+        GeneradorPeliculas gp3 = new GeneradorPeliculas();
+        gp2.establecerLlave(api3);
+        gp2.establecerUrl("http://api.movie?api=");
+        System.out.println(gp3.obtenerUrl());
+
     }
 }
